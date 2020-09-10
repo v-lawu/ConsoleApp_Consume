550a4065c6fbb91c5d59bfb6bdaf5ea2a8141790
Lan Wu (CSI Interfusion Inc) <v-lawu@microsoft.com>
2020-09-09 19:09:56 -0700

    Add project files.

---
+++ ConsoleApp_Consume/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Net.Http;
+
+namespace ConsoleApp_Consume
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PostStudent();
+            Console.WriteLine();
+            GetStudents();       
+        }
+
+        public static void GetStudents()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44341/api/");
+                //HTTP GET
+                var responseTask = client.GetAsync("Students");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+
+                    var readTask = result.Content.ReadAsAsync<Student[]>();
+                    readTask.Wait();
+
+                    var students = readTask.Result;
+
+                    foreach (var student in students)
+                    {
+                        Console.WriteLine(student.Name + "  " + student.Age);
+                    }
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Test");
+            Console.ReadLine();
+        }
+
+        public static void PostStudent()
+        {
+            var student = new Student() { Name = "Mei", Age = 14 };
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44341/api/");
+            var postTask = client.PostAsJsonAsync<Student>("Students", student);
+            postTask.Wait();
+
+            var result = postTask.Result;
+            if (result.IsSuccessStatusCode)
+            {
+
+                var readTask = result.Content.ReadAsAsync<Student>();
+                readTask.Wait();
+
+                var insertedStudent = readTask.Result;
+
+                Console.WriteLine("Student {0} inserted with id: {1}", insertedStudent.Name, insertedStudent.Id);
+            }
+            else
+            {
+                Console.WriteLine(result.StatusCode);
+            }
+        }
+    }
+}
---
+++ ConsoleApp_Consume/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp_Consume
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
